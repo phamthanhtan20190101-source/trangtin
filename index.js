312c974a423dec3fa2cf0eb74745c34b3c05f578
Tan <phamthanhtan20190101@gmail.com>
2026-04-07 19:02:13 +0700

    sửa lỗi

--- index.js
+++ index.js
@@ -56,5 +56,5 @@ app.use('/taikhoan', taikhoanRouter);
 app.use('/baiviet', baivietRouter);
 
 app.listen(3000, () => {
-	console.log('Server is running at http://127.0.0.1:3000');
+	console.log('Server is running at ');
 });
